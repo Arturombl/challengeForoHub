ce57ce99909e405985901fcc7f4b12d4d2f19c8e
Arturombl <arturomobl@gmail.com>
2024-07-14 00:55:37 -0700

    Iniciamos la API

---
+++ src/test/java/com/alura/cursos/challenge/foro/hub/HubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alura.cursos.challenge.foro.hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
